524b18a6d7e9b75ef013ec92002bba172ca8e2f8
Ayush <Ayush@DESKTOP-F3C4IRT>
2022-12-28 07:48:00 -0800

    comit

--- Class1.cs
+++ Class1.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Class1
+public class Class1:Claas2
 {
 	public Class1()
 	{
@@ -12,5 +12,13 @@ public class Class1
 	static void Main()
 	{
 		Class1 obj=new Class1();
+		obj.Show();
+	}
+}
+public class Claas2
+{
+	public void Show()
+	{
+		global::System.Console.WriteLine("Called");
 	}
 }
